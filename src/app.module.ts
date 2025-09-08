9d3c24c714ab8b9bd9bf12d868e6ce480b904bf0
filipee048 <filipejeremis692@gmail.com>
2025-09-08 10:42:24 -0300

    modulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ProductService } from './service/ProductService'
+import { CartService } from './service/CartService'
+import { CartController } from './controller/CartController'
 import { ProductResolver } from './app.resolver'
 import { GraphQLModule } from '@nestjs/graphql'
-import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { join } from 'path'
 
 @Module({
@@ -15,7 +18,7 @@ import { join } from 'path'
       sortSchema: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService, ProductService, ProductResolver],
+  controllers: [AppController, CartController],
+  providers: [AppService, ProductService, CartService, ProductResolver],
 })
 export class AppModule {}
